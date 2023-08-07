61ce15ea8fc0457c3ea055fa6e976a560f44adb0
Filippo Vissani <filippo.vissani@studio.unibo.it>
2023-08-07 15:23:42 +0200

    test: improve tests structure

---
+++ test/Homework1Spec.hs
@@ -0,0 +1,44 @@
+module Homework1Spec where
+
+import Test.HUnit
+import Homework1
+
+testToDigits1 :: Test
+testToDigits1 = TestCase (assertEqual "should be [1,2,3,4]" [1,2,3,4] (toDigits 1234))
+
+testToDigits2 :: Test
+testToDigits2 = TestCase (assertEqual "should be []" [] (toDigits 0))
+
+testToDigits3 :: Test
+testToDigits3 = TestCase (assertEqual "should be []" [] (toDigits (-17)))
+
+testToDigitsRev :: Test
+testToDigitsRev = TestCase (assertEqual "should be [4,3,2,1]" [4,3,2,1] (toDigitsRev 1234))
+
+testDoubleEveryOther1 :: Test
+testDoubleEveryOther1 = TestCase (assertEqual "should be [16,7,12,5]" [16,7,12,5] (doubleEveryOther [8,7,6,5]))
+
+testDoubleEveryOther2 :: Test
+testDoubleEveryOther2 = TestCase (assertEqual "should be [1,4,3]" [1,4,3] (doubleEveryOther  [1,2,3]))
+
+testSumDigits :: Test
+testSumDigits = TestCase (assertEqual "should be 1 + 6 + 7 + 1 + 2 + 5" (1 + 6 + 7 + 1 + 2 + 5) (sumDigits [16,7,12,5]))
+
+testValidate1 :: Test
+testValidate1 = TestCase (assertEqual "should be True" True (validate 4012888888881881))
+
+testValidate2 :: Test
+testValidate2 = TestCase (assertEqual "should be False" False (validate 4012888888881882))
+
+homework1Spec :: Test
+homework1Spec = TestList [
+    TestLabel "testToDigits1" testToDigits1,
+    TestLabel "testToDigits2" testToDigits2,
+    TestLabel "testToDigits3" testToDigits3,
+    TestLabel "testToDigitsRev" testToDigitsRev,
+    TestLabel "testDoubleEveryOther1" testDoubleEveryOther1,
+    TestLabel "testDoubleEveryOther2" testDoubleEveryOther2,
+    TestLabel "testSumDigits" testSumDigits,
+    TestLabel "testValidate1" testValidate1,
+    TestLabel "testValidate2" testValidate2
+    ]
--- test/Main.hs
+++ test/Main.hs
@@ -2,47 +2,10 @@ module Main where
 
 import Test.HUnit
 import qualified System.Exit as Exit
-import Homework1
-
-test1 :: Test
-test1 = TestCase (assertEqual "should be [1,2,3,4]" [1,2,3,4] (toDigits 1234))
-
-test2 :: Test
-test2 = TestCase (assertEqual "should be [4,3,2,1]" [4,3,2,1] (toDigitsRev 1234))
-
-test3 :: Test
-test3 = TestCase (assertEqual "should be []" [] (toDigits 0))
-
-test4 :: Test
-test4 = TestCase (assertEqual "should be []" [] (toDigits (-17)))
-
-test5 :: Test
-test5 = TestCase (assertEqual "should be [16,7,12,5]" [16,7,12,5] (doubleEveryOther [8,7,6,5]))
-
-test6 :: Test
-test6 = TestCase (assertEqual "should be [1,4,3]" [1,4,3] (doubleEveryOther  [1,2,3]))
-
-test7 :: Test
-test7 = TestCase (assertEqual "should be 1 + 6 + 7 + 1 + 2 + 5" (1 + 6 + 7 + 1 + 2 + 5) (sumDigits [16,7,12,5]))
-
-test8 :: Test
-test8 = TestCase (assertEqual "should be True" True (validate 4012888888881881))
-
-test9 :: Test
-test9 = TestCase (assertEqual "should be False" False (validate 4012888888881882))
+import Homework1Spec
 
 tests :: Test
-tests = TestList [
-    TestLabel "test1" test1,
-    TestLabel "test2" test2,
-    TestLabel "test3" test3,
-    TestLabel "test4" test4,
-    TestLabel "test5" test5,
-    TestLabel "test6" test6,
-    TestLabel "test7" test7,
-    TestLabel "test8" test8,
-    TestLabel "test9" test9
-    ]
+tests = homework1Spec
 
 main :: IO()
 main = do
